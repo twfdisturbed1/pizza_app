7bb1596b81ef1c0ff77a9eaafda3519d91047c70
William Mullins <twfdisturbed@hotmail.com>
2018-06-13 14:16:48 -0400

    stage 1

---
+++ pizza_app.rb
@@ -0,0 +1,10 @@
+meats = [ "pepperoni", "sasauge", "bacon", "ham"]
+veggies = [ "peppers", "tomatoes", "onions"]
+crust = [ "thin crust", "thick crust", "stuffed crust"]
+sauce = [ "marinara sauce", "no sauce", "butter sauce" ]
+size = [ "small", "medium", "large", "extra_large"]
+
+
+puts "One #{size.sample} #{crust.sample} with #{sauce.sample} topped with #{meats.sample} and #{veggies.sample}"
+
+
